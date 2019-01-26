c8dc7c7f974e348bb4ef1cfcddef32ac22e3a353
Jesse Mills <jessemills1989@gmail.com>
2019-01-26 13:42:30 -0500

    Adding initial scala exercise files

---
+++ src/com/jessermills/scalaexercises/Exercises/PracticeProblems.scala
@@ -0,0 +1,73 @@
+package com.jessermills.Exercises
+
+import collection.mutable.HashMap
+
+object PracticeProblems {
+
+  def transformWords(words: List[String]) = {
+
+    val hashMap = new HashMap[String, Boolean]()
+    words
+      .flatMap(shortenWordValidatingPreviousAcronyms(hashMap, _))
+      .foreach(str => println(str))
+  }
+
+  private def shortenWordValidatingPreviousAcronyms(map: HashMap[String, Boolean], word: String) : Option[String] = {
+
+    if (word.isEmpty)
+      None
+    else
+      recursiveShortenWord(
+        map = map,
+        originalWord = word,
+        shortenedWord = shortenWord(
+          word = word,
+          offset = 0),
+        offset = 0
+      )
+
+  }
+
+  private def recursiveShortenWord(map: HashMap[String, Boolean], originalWord: String, shortenedWord: String, offset: Int): Option[String] = {
+
+    map.contains(shortenedWord) match {
+      case false =>
+        map.put(shortenedWord, true)
+        Some(shortenedWord)
+      case true =>
+
+        val newOffset = offset + 1
+
+        if (newOffset < originalWord.length) {
+
+          val newShortenedWord = shortenWord(originalWord, newOffset)
+
+          map.contains(newShortenedWord) match {
+            case true => recursiveShortenWord(map, originalWord, newShortenedWord, newOffset)
+            case false =>
+              map.put(newShortenedWord, true)
+              Some(newShortenedWord)
+          }
+
+        } else {
+          None
+        }
+
+    }
+
+  }
+
+  private def shortenWord(word: String, offset: Int) : String = {
+
+    val len = word.length
+    val numberOffset = offset + 2
+    val lastCharIndex = len - 1
+
+    if (numberOffset >= lastCharIndex)
+      word
+    else {
+      s"${word.substring(0, offset + 1) + (len - numberOffset) + word.charAt(lastCharIndex)}"
+    }
+  }
+
+}
---
+++ src/com/jessermills/scalaexercises/Hello.scala
@@ -0,0 +1,16 @@
+package com.jessermills
+
+import Exercises._
+
+object Hello extends App {
+
+  val listOfWords = List[String](
+    xs = "internationalization",
+    "internationalization",
+    "aaa",
+    "aaa",
+    "bingBop",
+    "")
+
+  PracticeProblems.transformWords(listOfWords)
+}
